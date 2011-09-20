90134341328f64f1dfdc69a7af5298ded68871eb
unknown <andrew@.(none)>
2011-09-20 13:21:15 +0100

    can now create an email relayer from configuration via factory method

--- EmailProcessing/EmailSenderFactory.cs
+++ EmailProcessing/EmailSenderFactory.cs
@@ -11,5 +11,12 @@ namespace EmailProcessing
             IEmailSender sender = Activator.CreateInstance(senderType, configuration) as IEmailSender;
             return sender;
         }
+
+        public static IEmailPackageRelayer CreateRelayerFromConfiguration(EmailBuilderConfigurationSection configuration)
+        {
+            Type relayerType = Type.GetType(configuration.EmailSenderType.Type);
+            IEmailPackageRelayer relayer = Activator.CreateInstance(relayerType, configuration) as IEmailPackageRelayer;
+            return relayer;
+        }
     }
 }
