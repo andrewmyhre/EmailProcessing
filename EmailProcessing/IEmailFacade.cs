ea351140659e924927233b1edc261838d17e2bc0
Andrew Myhre <andrew.myhre@gmail.com>
2012-04-10 00:47:07 +0100

    set default email culture to polish

--- EmailProcessing/IEmailFacade.cs
+++ EmailProcessing/IEmailFacade.cs
@@ -5,11 +5,11 @@ namespace EmailProcessing
 {
     public interface IEmailFacade
     {
-        void Send<T>(string to, string templateName, T model, string culture = "en");
-        void Send<T>(string[] to, string templateName, T model, FileInfo[] fileAttachments, string culture = "en");
+        void Send<T>(string to, string templateName, T model, string culture = "pl");
+        void Send<T>(string[] to, string templateName, T model, FileInfo[] fileAttachments, string culture = "pl");
 
-        void Send(string to, string templateName, Dictionary<string, string> tokenReplacements, string culture = "en");
-        void Send(string[] to, string templateName, Dictionary<string, string> tokenReplacements, string culture = "en");
+        void Send(string to, string templateName, Dictionary<string, string> tokenReplacements, string culture = "pl");
+        void Send(string[] to, string templateName, Dictionary<string, string> tokenReplacements, string culture = "pl");
         void Send(string[] to, string templateName, Dictionary<string,string> tokenReplacements, FileInfo[] fileAttachments, string culture = "en");
 
         void LoadTemplates();
