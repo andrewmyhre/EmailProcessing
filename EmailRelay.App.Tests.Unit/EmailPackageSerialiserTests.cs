463151d0c357e9a83e8b62441c7393a45ad0ad05
Andrew Myhre <andrew.myhre@gmail.com>
2012-04-13 15:23:43 +0100

    fixes broken unit tests

--- EmailRelay.App.Tests.Unit/EmailPackageSerialiserTests.cs
+++ EmailRelay.App.Tests.Unit/EmailPackageSerialiserTests.cs
@@ -22,11 +22,12 @@ namespace EmailRelay.App.Tests.Unit
             Assert.That(package.From, Is.EqualTo("testfrom@server.com"));
             Assert.That(package.Subject, Is.EqualTo("test email subject: {subjectToken}"));
             Assert.That(package.To, Has.Count.EqualTo(1));
-            Assert.That(package.To, Has.Some.StringMatching("testrecipient@test.com"));
+            Assert.That(package.To, Has.Some.InstanceOf<Recipient>().With.Property("EmailAddress").StringMatching("testrecipient@test.com"));
             Assert.That(package.Html.Trim(), Is.StringContaining(@"<p>html body</p>"));
             Assert.That(package.Text.Trim(), Is.StringContaining(@"text body"));
             Assert.That(package.Attachments, Has.Count.EqualTo(1));
-            Assert.That(package.Attachments, Has.Some.StringContaining("D:\\workspace\\git\\EmailSystem\\EmailRelay.App.Tests.Unit\\attachment.jpg"));
+            Assert.That(package.Attachments,
+                        Has.Some.InstanceOf<Attachment>().With.Property("Path").StringContaining(@"attachment.jpg"));
         }
 
         [Test]
@@ -59,17 +60,17 @@ namespace EmailRelay.App.Tests.Unit
         public void EmailPackageSerialiser_CanSerialiseAndDeserialiseIntoEqualObject()
         {
             EmailPackageSerialiser serialiser = new EmailPackageSerialiser();
-            var package = serialiser.Deserialize(File.ReadAllText("SampleEmailPackage.xml"));
-            var serialised = serialiser.Serialise(package);
-            var deserialised = serialiser.Deserialize(serialised);
+            var expected = serialiser.Deserialize(File.ReadAllText("SampleEmailPackage.xml"));
+            var serialised = serialiser.Serialise(expected);
+            var actual = serialiser.Deserialize(serialised);
 
-            foreach(var propertyInfo in typeof(EmailPackage).GetProperties())
-            {
-                object expected = propertyInfo.GetValue(package, null);
-                object actual = propertyInfo.GetValue(deserialised, null);
-
-                Assert.AreEqual(expected, actual);
-            }
+            Assert.That(actual.To[0].EmailAddress, Is.EqualTo(expected.To[0].EmailAddress));
+            Assert.That(actual.To[0].Name, Is.EqualTo(expected.To[0].Name));
+           Assert.That(actual.Attachments[0].Path, Is.EqualTo(expected.Attachments[0].Path));
+            Assert.That(actual.From, Is.EqualTo(expected.From));
+            Assert.That(actual.Subject, Is.EqualTo(expected.Subject));
+            Assert.That(actual.Html, Is.EqualTo(expected.Html));
+            Assert.That(actual.Text, Is.EqualTo(expected.Text));
         }
     }
 }
